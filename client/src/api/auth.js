7e5751960bda7553585a258e421f2b3b6b361e8d
abedpiedra <a.piedracastillo@uandresbello.edu>
2025-05-26 21:28:45 -0400

    actualización

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API = "http://localhost:4000/api";
 
 export const registerRequest = (user) => axios.post(`${API}/register`, user);
-
+// This function sends a POST request to the API to register a new user.
 export const loginRequest = user => axios.post(`${API}/login`, user)
-
 
